c62269e92fe058805ab25caf0f8564b1a42791e3
Birthman <84112536+Birthman@users.noreply.github.com>
2025-06-08 06:59:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from dotenv import load_dotenv
 load_dotenv()
 from flask import Flask
 from app.routes.wish import wish_bp
+from app.routes.metadata import metadata_bp  # <-- Add this line
 
 app = Flask(__name__, template_folder="templates")
 
 app.register_blueprint(wish_bp)
+app.register_blueprint(metadata_bp)  # <-- And this line
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+
 
