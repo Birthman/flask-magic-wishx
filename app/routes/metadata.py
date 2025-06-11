3ac427a2cf2db87998eaeb5e35749284771cece3
Birthman <84112536+Birthman@users.noreply.github.com>
2025-06-11 06:13:07 -0700

    Update metadata.py

--- app/routes/metadata.py
+++ app/routes/metadata.py
@@ -2,6 +2,10 @@ from flask import Blueprint, send_from_directory
 
 metadata_bp = Blueprint("metadata", __name__)
 
+@metadata_bp.route("/.well-known/xrp-ledger.toml")
+def serve_toml():
+    return send_from_directory(".well-known", "xrp-ledger.toml", mimetype='text/plain')
+
 @metadata_bp.route("/xmc.json")
 def serve_xmc_metadata():
     return send_from_directory("static/metadata", "xmc.json")
