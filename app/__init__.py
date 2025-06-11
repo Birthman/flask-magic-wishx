3e815e077556f342e4f7075d163955af240ae0e3
Birthman <84112536+Birthman@users.noreply.github.com>
2025-06-11 06:14:56 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,12 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    # Import and register Blueprints
+    # Register wish routes
     from .routes.wish import wish_bp
     app.register_blueprint(wish_bp)
+
+    # Register metadata routes
+    from .metadata import metadata_bp
+    app.register_blueprint(metadata_bp)
 
     return app
